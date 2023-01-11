0d4d8b4d82293bcee269d91aeec01167b31aba05
JuliusBreuss <julius.breuss@stud.fh-campuswien.ac.at>
2023-01-11 16:22:33 +0100

    test

--- Map.cs
+++ Map.cs
@@ -14,13 +14,15 @@ namespace ProjectTank
     internal class Map
     {
         Texture2D sprite;
+        Texture2D border;
         Vector2 position;
         Vector2 dimension;
 
         
-        public Map(Texture2D sprite)
+        public Map(Texture2D sprite,Texture2D border)
         {
-            this.sprite = sprite;  
+            this.sprite = sprite;
+            this.border = border;
 
         }
 
@@ -32,6 +34,18 @@ namespace ProjectTank
                 {
                     spriteBatch.Draw(sprite, new Rectangle(i, j, 32, 32), Color.White);
                 }
+            }
+            for (int i = 0; i < 2016; i = i + 32)
+            {
+                if (i >= 1200)
+                {
+                    spriteBatch.Draw(border, new Rectangle(0, i - 1216, 32, 32), Color.White);
+                    spriteBatch.Draw(border, new Rectangle(1184, i - 1216, 32, 32), Color.White);
+                }
+
+                spriteBatch.Draw(border, new Rectangle(i, 768, 32, 32), Color.White);
+                spriteBatch.Draw(border, new Rectangle(i, 0, 32, 32), Color.White);
+
             }
         }
         
